9bec5e3096be1fe0ecb16b76b3a1386a2cc60860
Gabriel Andretta <ohhgabriel@gmail.com>
2013-06-04 21:54:35 -0300

    Ensure packages for Clojure development presence

--- init.el
+++ init.el
@@ -2,3 +2,13 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(clojure-mode clojure-test-mode nrepl)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
