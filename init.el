197d1120903bf0a1fb03372c91d59e5980dc4c6f
Gabriel Andretta <gabriel.andretta@gmail.com>
2021-02-13 19:35:49 -0300

    Load user config file with customizations

--- init.el
+++ init.el
@@ -2,3 +2,4 @@
 					     (buffer-file-name))))
 (setq config-file-path (expand-file-name "config.org" user-emacs-directory))
 (org-babel-load-file config-file-path)
+(load (expand-file-name "user.el" user-emacs-directory) 'noerror)
